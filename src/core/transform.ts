4452b1a497abb86c1289e3cc75d675aebf969b50
李铭辉 <1018596741@qq.com>
2020-07-17 23:16:21 +0800

    feat(src/core/transform.ts): 完成对cjs模式的常量require移除逻辑

--- src/core/transform.ts
+++ src/core/transform.ts
@@ -1,6 +1,6 @@
 import traverse,{Visitor} from "@babel/traverse";
 import {ModuleListType} from "./validate";
-import { File } from "@babel/types";
+import { File, Identifier } from "@babel/types";
 
 export const transform = (ast:File,type:"esm"|"cjs",moduleList:ModuleListType):void => {
     const targetModules:string[] = Object.keys(moduleList).filter((v:string)=>moduleList[v]===true);
@@ -21,6 +21,29 @@ function esmTransformer(targetModules:string[]):Visitor {
 }
 
 function cjsTransformer(targetModules:string[]):Visitor {
-    const visiter:Visitor = {};
+    const visiter:Visitor = {
+        VariableDeclaration(path){
+            const {declarations} = path.node;
+            Array.from(declarations).forEach(declarator => {
+                const {init} = declarator;
+                if(init?.type === "CallExpression"){
+                    const callee = init.callee as Identifier;
+                    if(callee.name&&callee.name === "require"){
+                        const {arguments:args} = init;
+                        const isTarget:boolean = args.some((v) => {
+                            if(v.type === "StringLiteral"&&targetModules.includes(v.value)===true){
+                                return true;
+                            }else{
+                                return false;
+                            }
+                        });
+                        if(isTarget){
+                            path.remove();
+                        }
+                    }
+                }
+            });
+        }
+    };
     return visiter;
 }

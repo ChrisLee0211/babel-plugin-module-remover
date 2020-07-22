a445b0ab6fdce04084a7afae1d6165f81be2927f
李铭辉 <1018596741@qq.com>
2020-07-22 23:29:05 +0800

    chore(rollup.config.js): rollup配置重写，增加node-modules打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
 import typescript from "rollup-plugin-typescript";
 import sourceMaps from "rollup-plugin-sourcemaps";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 import { eslint } from 'rollup-plugin-eslint';
 
 export default {
   input: "./src/main.ts",
   plugins: [
+    resolve(),
+    commonjs(),
+    json(),
     eslint({
       throwOnError: true,
       throwOnWarning: false,
       include: ['src/**'],
       exclude: ['node_modules/**']
-  }),
+    }),
     typescript({
       exclude: "node_modules/**",
       typescript: require("typescript")

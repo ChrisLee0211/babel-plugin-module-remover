685ce9920e569fdb172a3c9c0b2b016aa06e4909
李铭辉 <1018596741@qq.com>
2020-08-07 22:51:32 +0800

    完成babel插件基本改造

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
 import core from "./core";
 import { Visitor } from "@babel/core";
+import {isArray} from "./utils";
+import { Identifier } from "@babel/types";
+interface Options {
+    type: "esm" | "cjs",
+    target:string[]
+}
+
+const optionValidate = (opt:Options): Options => {
+    const optionsKeys:string[] = Object.keys(opt);
+    const opts:Options = Object.create({});
+    if(optionsKeys.includes("type")){
+        opts["type"] = opt["type"];
+    }else{
+        opts["type"] = "esm";
+    }
+    if(optionsKeys.includes("target")){
+        if(isArray(opt["target"])){
+            opts["target"] = opt["target"];
+        }
+    }else{
+        throw Error(`the prop of option in @babel/module-remover expect a Array, but got undefined `);
+    }
+    return opts;
+};
 
 export default function({type:t}){
-    const visitor:Visitor<{opt:Array<number>}> = {
-        ImportDeclaration(path,state={opt}){
+    const visitor:Visitor<{opt:Options}> = {
+        ImportDeclaration(path,state){
+            const {target:targetModules} = optionValidate(state.opt);
             const {source} = path.node;
             if(targetModules.includes(source.value)){
                 path.remove();
             }
         },
-        VariableDeclaration(path){
+        VariableDeclaration(path,state){
             const {declarations} = path.node;
+            const {target:targetModules} = optionValidate(state.opt);
             Array.from(declarations).forEach(declarator => {
                 const {init} = declarator;
                 if(init?.type === "CallExpression"){
@@ -31,8 +57,9 @@ export default function({type:t}){
                 }
             });
         },
-        ExpressionStatement(path){
+        ExpressionStatement(path,state){
             const {expression} = path.node;
+            const {target:targetModules} = optionValidate(state.opt);
             if(expression.type === "CallExpression"){
                 const callee = expression.callee as Identifier;
                 if(callee.name&&callee.name === "require") {
